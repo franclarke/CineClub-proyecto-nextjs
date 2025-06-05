9c5dbc5f7a3c37cdffb1ad8835af182f3b260aec
Francisco <franclarke00@hotmail.com>
2025-06-05 12:25:00 -0300

    build

--- src/app/events/[id]/page.tsx
+++ src/app/events/[id]/page.tsx
@@ -1,9 +1,10 @@
 import { DataAccess } from './components/data-access'
 
-export default function EventDetailPage({ params }: { params: { id: string } }) {
+export default async function EventDetailPage({ params }: { params: Promise<{ id: string }> }) {
+	const { id } = await params
 	return (
 		<div className='min-h-screen bg-gradient-to-br from-deep-night to-black/90 py-12 px-4'>
-			<DataAccess id={params.id} />
+			<DataAccess id={id} />
 		</div>
 	)
 } 
