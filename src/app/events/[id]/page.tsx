5e12982a6d460cf2d227d9140f6f3dccf3f61296
Francisco <franclarke00@hotmail.com>
2025-06-05 11:33:10 -0300

    build errors fixed

--- src/app/events/[id]/page.tsx
+++ src/app/events/[id]/page.tsx
@@ -1,10 +1,6 @@
 import { DataAccess } from './components/data-access'
 
-interface EventDetailPageProps {
-  params: { id: string }
-}
-
-export default function EventDetailPage({ params }: EventDetailPageProps) {
+export default function EventDetailPage({ params }: { params: { id: string } }) {
   return (
     <div className="min-h-screen bg-gradient-to-br from-deep-night to-black/90 py-12 px-4">
       <DataAccess id={params.id} />
