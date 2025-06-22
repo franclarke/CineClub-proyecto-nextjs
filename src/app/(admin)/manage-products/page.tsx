d1d2bfd9ec7befbc81e1735350d1cbc8a6730373
Antonio <totocarlos0@outlook.com>
2025-06-22 19:21:44 -0300

    Gestion de Stock - Finalizada

---
+++ src/app/(admin)/manage-products/page.tsx
@@ -0,0 +1,9 @@
+import ProductsDashboard from './components/productsDashboard'
+
+export default function ManageProductsPage() {
+    return (
+        <div className="min-h-screen p-4 bg-gradient-to-br from-gray-900 via-gray-800 to-orange-900 py-8">
+            <ProductsDashboard />
+        </div>
+    )
+}
